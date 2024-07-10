1b6f433a5ff647c9a382b2c2c8111062379b905e
RaviChandraYalla <raviyalla07@gmail.com>
2024-07-09 21:17:42 -0500

    new first commit

---
+++ Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String args[]){
+
+        System.out.println("third msg from new class");
+
+        System.out.println("fourth msg from new class");
+    }
+}
